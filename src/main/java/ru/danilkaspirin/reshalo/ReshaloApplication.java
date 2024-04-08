b5223c88cb7d5193991232519b9b46c2e1858757
Danil <danilkaspirin@gmail.com>
2024-04-08 22:14:58 +1000

    init commit

---
+++ src/main/java/ru/danilkaspirin/reshalo/ReshaloApplication.java
@@ -0,0 +1,13 @@
+package ru.danilkaspirin.reshalo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReshaloApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReshaloApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/danilkaspirin/reshalo/ReshaloApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.danilkaspirin.reshalo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReshaloApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
